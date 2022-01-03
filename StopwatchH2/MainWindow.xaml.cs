aab181d2ce3988b91c9e7613d39d4417ed157405
Phillip Svanholt Kjær Jepsen <jepsens123@outlook.dk>
2022-01-03 14:13:20 +0100

    Add project files.

---
+++ StopwatchH2/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace StopwatchH2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Thread thread;
+        Dispatcher dispatcher;
+        Timer timer;
+        public MainWindow()
+        {
+            dispatcher = Application.Current.MainWindow.Dispatcher;
+
+            timer = new Timer();
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            thread = new Thread(TimerRun);
+            thread.Start();
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            timer.IsFinished = true;
+            CountLabel.Content = timer.ShowElapsedTime();
+            timer.StopTimer();
+            thread.Join();
+        }
+        private void TimerRun()
+        {
+            timer.IsFinished = false;
+            timer.StartTimer();
+            while (!timer.IsFinished)
+            {
+                dispatcher.BeginInvoke(new Action(() =>
+                {
+                    CountLabel.Content = timer.GetRemaningTimeLeft(Convert.ToInt32(NumberFromTextBox.Text));
+                }));
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ StopwatchH2/Timer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopwatchH2
+{
+    public class Timer
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        public bool IsFinished { get; set; } = false;
+        public int Count { get; private set; } = 0;
+        public void StartTimer()
+        {
+            stopwatch.Start();
+        }
+        public int GetRemaningTimeLeft(int number)
+        {
+            if (stopwatch.Elapsed.Minutes == number)
+            {
+                IsFinished = true;
+                TimerFinished();
+                return number * 60;
+            }
+            else
+            {
+                Count++;
+                return Count;
+            }
+        }
+        public void StopTimer()
+        {
+            stopwatch.Stop();
+        }
+        public int ShowElapsedTime()
+        {
+            return stopwatch.Elapsed.Seconds;
+        }
+        public void TimerFinished()
+        {
+            stopwatch.Stop();
+        }
+    }
+}
